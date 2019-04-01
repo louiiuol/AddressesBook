87fa6e59bd2115e6e3ec9ab8b7f589cd3dc103dd
Louis Godlewski <louis.godlewski@gmail.com>
2019-04-01 16:45:15 +0200

    ADBOOK-003 - Ajout du Js

--- AddressBook-Front/js/app.js
+++ AddressBook-Front/js/app.js
@@ -1,6 +1,42 @@
-let loader = document.getElementById('loader');
+const loader = document.getElementById('loader');
+const notif = document.getElementById('notif');
+const url = "http://localhost:8082/addressBook/loading/";
 loader.onclick = () => {
-    alert("sending order: HOLD THE DOOR !");
-    // Envoie requete http get à  l'adresses ci dessous 
-    // http://localhost:8082/addressesBook/loader/
+    client = new XMLHttpRequest();
+    client.onload = function (data) {
+
+        if (client.readyState == 4 && client.status == "200") {
+            console.table(client);
+            console.log("Adresses Loaded");
+            //Append to field
+            //<p class="help is-success">This username is available</p>
+            //  <p class="help is-danger">This email is invalid</p>
+            // <span class="icon is-small is-left"><i class="fas fa-envelope"></i></span>
+        }
+        
+        else {
+            console.error(data);
+             // Append to field
+        //  <p class="help is-danger">This email is invalid</p>
+        // <span class="icon is-small is-left"><i class="fas fa-envelope"></i></span>
+        }
+      }
+    
+    client.open('GET', url, true);
+    client.setRequestHeader('Content-type','charset=utf-8');
+
+    client.send(); 
 };
+notif.hidden = true;
+notif.innerText = "toto";
+notif.hidden = false;
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+      $notification = $delete.parentNode;
+      $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification);
+      });
+    });
+  });
