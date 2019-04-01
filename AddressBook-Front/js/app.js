52a5b46001b1502d87e12d1adf08b2f611be83d1
Louis Godlewski <louis.godlewski@gmail.com>
2019-04-01 13:02:40 +0200

    ADBOOK-002 - Ajout du Front

---
+++ AddressBook-Front/js/app.js
@@ -0,0 +1,6 @@
+let loader = document.getElementById('loader');
+loader.onclick = () => {
+    alert("sending order: HOLD THE DOOR !");
+    // Envoie requete http get à  l'adresses ci dessous 
+    // http://localhost:8082/addressesBook/loader/
+};
