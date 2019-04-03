5112a5120d5abd3e7b22b9ab03c8bf5bb5950091
Nathalie ROBERT <robert.nathaliejm@gmail.com>
2019-04-03 12:27:21 +0200

    refacto removeAll

--- AddressBook-Back/src/main/java/fr/simplon/addressBook/repository/AddressJpaRepository.java
+++ AddressBook-Back/src/main/java/fr/simplon/addressBook/repository/AddressJpaRepository.java
@@ -11,6 +11,10 @@ import fr.simplon.addressBook.entities.Address;
 
 public interface AddressJpaRepository extends JpaRepository<Address, Long> {
 
+    @Query("delte")
+    @Modifying
+    void removeAll();
+
 //    @Query("delete")
 //    @Modifying
 //    void removeAll();
--- AddressBook-Back/src/main/java/fr/simplon/addressBook/services/impl/AddressServiceImpl.java
+++ AddressBook-Back/src/main/java/fr/simplon/addressBook/services/impl/AddressServiceImpl.java
@@ -62,8 +62,7 @@ public class AddressServiceImpl implements AddressService {
 	if(repoAddress.count() == 0 ) {
 	    repoAddress.saveAll(parse());
 	} else {
-	   // repoAddress.removeAll();
-	    repoAddress.deleteAll();
+	    repoAddress.removeAll();
 	    repoAddress.saveAll(parse());
 	}
 	
