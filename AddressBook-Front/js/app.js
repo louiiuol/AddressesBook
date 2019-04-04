016fcf1df31486de2fa9f5a2181c55009a2be5fe
Nathalie ROBERT <robert.nathaliejm@gmail.com>
2019-04-04 13:01:06 +0200

    create message depending success/error

--- AddressBook-Front/js/app.js
+++ AddressBook-Front/js/app.js
@@ -1,13 +1,27 @@
 const loader = document.getElementById('loader');
 const notif = document.getElementById('notif');
-const url = "http://localhost:8082/addressBook/loading/";
+const url = "http://localhost:8082/AddressBook/loading/";
+
+const loadingSpinner = document.createElement("i");
+loadingSpinner.classList.add("fas", "fa-spinner", "fa-pulse");
+const successMsg = "Les addresses ont bien été chargé depuis le csv.";
+const errorMsg = "Un problème est survenu dans le serveur, contacter l'administrateur.";
+
+
+notif.hidden = true;
+
 loader.onclick = () => {
     client = new XMLHttpRequest();
+    notif.append(loadingSpinner);
+    notif.hidden = false;
     client.onload = function (data) {
-
+    
         if (client.readyState == 4 && client.status == "200") {
             console.table(client);
             console.log("Adresses Loaded");
+            notif.innerText = successMsg;
+            notif.classList.add("is-success");
+            
             //Append to field
             //<p class="help is-success">This username is available</p>
             //  <p class="help is-danger">This email is invalid</p>
@@ -16,6 +30,8 @@ loader.onclick = () => {
         
         else {
             console.error(data);
+            notif.innerText = errorMsg ;
+            notif.classList.add("is-danger");
              // Append to field
         //  <p class="help is-danger">This email is invalid</p>
         // <span class="icon is-small is-left"><i class="fas fa-envelope"></i></span>
@@ -27,9 +43,9 @@ loader.onclick = () => {
 
     client.send(); 
 };
-notif.hidden = true;
-notif.innerText = "toto";
-notif.hidden = false;
+
+
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
