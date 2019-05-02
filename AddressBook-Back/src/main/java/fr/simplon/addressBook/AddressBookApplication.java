892e919f05c42fe034b745c5ad02bd18a78891a7
Nathalie ROBERT <robert.nathaliejm@gmail.com>
2019-05-02 17:58:08 +0200

    ADBK#14: ModelMapper config + Pagination

--- AddressBook-Back/src/main/java/fr/simplon/addressBook/AddressBookApplication.java
+++ AddressBook-Back/src/main/java/fr/simplon/addressBook/AddressBookApplication.java
@@ -1,7 +1,9 @@
 package fr.simplon.addressBook;
 
 import org.modelmapper.ModelMapper;
-
+import org.modelmapper.config.Configuration.AccessLevel;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,15 +14,21 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class AddressBookApplication {
 
-	private static final Logger logger = LoggerFactory.getLogger(AddressBookApplication.class);
+    private static final Logger logger = LoggerFactory
+	    .getLogger(AddressBookApplication.class);
 
-	public static void main(String[] args) {
-		logger.info("Starting Spring Boot Application....");
-		SpringApplication.run(AddressBookApplication.class, args);
-		logger.info("Application Started!!");
-	}
+    public static void main(String[] args) {
+	logger.info("Starting Spring Boot Application....");
+	SpringApplication.run(AddressBookApplication.class, args);
+	logger.info("Application Started!!");
+    }
 
-	@Bean
-	public ModelMapper mapper() { return new ModelMapper(); }
-
+    @Bean
+    public ModelMapper mapper() {
+	ModelMapper mapper = new ModelMapper();
+	mapper.getConfiguration().setFieldMatchingEnabled(true)
+		.setFieldAccessLevel(AccessLevel.PRIVATE)
+		.setMatchingStrategy(MatchingStrategies.STRICT);
+	return mapper;
+    }
 }
